d2638bc2f3f73d3598f2584db4f6be6c06c6eb4b
Shaila Nasrin <drshaila2021@gmail.com>
2022-07-20 21:35:45 -0500

    banner created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { Routes } from 'react-router-dom';
-import './App.css';
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Banner from "./components/Home/Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-4xl text-center bg-primary text-red-500'>Pioneerflix</h1>
-      <Routes>
-
-      </Routes>
+      <h1 className="text-4xl text-center bg-primary text-red-500">
+        Pioneerflix
+      </h1>
+      <Banner></Banner>
+      <Routes></Routes>
     </div>
   );
 }
