5b5f52c4a550a2239d5a8d29774c1f985b06614c
MagicManik <manikislam1952@gmail.com>
2022-07-24 23:54:28 +0600

    add new section on home page

--- src/components/Home/PioneerflixFree/PioneerflixFree.js
+++ src/components/Home/PioneerflixFree/PioneerflixFree.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import movie1 from '../../../assets/bangla-movie/movie (1).jpg';
-import movie2 from '../../../assets/bangla-movie/movie (2).jpg';
-import movie3 from '../../../assets/bangla-movie/movie (3).jpg';
-import movie4 from '../../../assets/bangla-movie/movie (4).jpg';
-import movie5 from '../../../assets/bangla-movie/movie (5).jpg';
-import movie6 from '../../../assets/bangla-movie/movie (6).jpg';
-import offlineWatch from '../../../assets/others/watch-off.jpg';
-import './MostPopular.css';
+import movie1 from '../../../assets/free-videos/free-video (1).jpg';
+import movie2 from '../../../assets/free-videos/free-video (2).jpg';
+import movie3 from '../../../assets/free-videos/free-video (3).jpg';
+import movie4 from '../../../assets/free-videos/free-video (4).jpg';
+import movie5 from '../../../assets/free-videos/free-video (5).jpg';
+import movie6 from '../../../assets/free-videos/free-video (6).jpg';
+import movie7 from '../../../assets/free-videos/free-video (7).jpg';
+import freeVideo from '../../../assets/others/free-video.jpg';
+import './PioneerflixFree.css';
 
-const MostPopular = () => {
+const PioneerflixFree = () => {
 
     const popularMovies = [
         {
@@ -46,6 +47,12 @@ const MostPopular = () => {
             name: 'Movie 6',
             description: '',
             img: movie6
+        },
+        {
+            _id: 7,
+            name: 'Movie 7',
+            description: '',
+            img: movie7
         }
     ]
 
@@ -53,8 +60,8 @@ const MostPopular = () => {
         <section className='bg-secondary pb-3'>
             <div className='bg-primary video-section'>
                 <div className='bg-primary lg:px-20 sm:px-4 video-container'>
-                    <h1 className='text-2xl text-white py-6'>PIONEERFLIX MOST POPULAR</h1>
-                    <div className='grid lg:grid-cols-6 gap-4 popular-movie-section'>
+                    <h1 className='text-2xl text-white py-6'>PIONEERFLIX FREE</h1>
+                    <div className='grid lg:grid-cols-7 gap-4 popular-movie-section'>
 
                         {
                             popularMovies.map(movie =>
@@ -67,10 +74,10 @@ const MostPopular = () => {
                 </div>
             </div>
 
-            <div className='text-container bg-primary grid lg:grid-cols-2 gap-4 items-center sm:grid-cols-1'>
+            <div className='text-container-2 bg-primary grid lg:grid-cols-2 gap-4 items-center sm:grid-cols-1'>
 
                 <div>
-                    <img src={offlineWatch} alt="" />
+                    <img className='w-11/12 block mx-auto' src={freeVideo} alt="" />
                 </div>
                 <div className='text-container-right'>
                     <h1 className='text-5xl font-bold text-white'>Download your shows to watch offline.</h1>
@@ -82,4 +89,4 @@ const MostPopular = () => {
     );
 };
 
-export default MostPopular;
+export default PioneerflixFree;
--- src/components/Home/PioneerflixKids/PioneerflixKids.js
+++ src/components/Home/PioneerflixKids/PioneerflixKids.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import movie1 from '../../../assets/tvchannels/tv-chaneel (1).png';
-import movie2 from '../../../assets/tvchannels/tv-chaneel (2).png';
-import movie3 from '../../../assets/tvchannels/tv-chaneel (3).png';
-import movie4 from '../../../assets/tvchannels/tv-chaneel (4).png';
-import movie5 from '../../../assets/tvchannels/tv-chaneel (5).png';
-import movie6 from '../../../assets/tvchannels/tv-chaneel (6).png';
-import movie7 from '../../../assets/tvchannels/tv-chaneel (7).png';
-import tv from '../../../assets/others/tv.png';
-import './TvChannels.css';
+import movie1 from '../../../assets/kids-videos/kids (1).webp';
+import movie2 from '../../../assets/kids-videos/kids (10).webp';
+import movie3 from '../../../assets/kids-videos/kids (3).webp';
+import movie4 from '../../../assets/kids-videos/kids (4).webp';
+import movie5 from '../../../assets/kids-videos/kids (5).webp';
+import movie6 from '../../../assets/kids-videos/kids (6).webp';
+import movie7 from '../../../assets/kids-videos/kids (7).webp';
+import movie8 from '../../../assets/kids-videos/kids (8).webp';
+import profileKids from '../../../assets/others/profile-kids.png';
 
-const TvChannels = () => {
+const PioneerflixKids = () => {
 
     const popularMovies = [
         {
@@ -56,19 +56,18 @@ const TvChannels = () => {
         },
         {
             _id: 8,
-            name: 'Movie 7',
+            name: 'Movie 8',
             description: '',
-            img: movie7
+            img: movie8
         }
     ]
 
     return (
-
         <section className='bg-secondary pb-3'>
             <div className='bg-primary video-section'>
                 <div className='bg-primary lg:px-20 sm:px-4 video-container'>
-                    <h1 className='text-2xl text-white py-6'>TV CHANNELS</h1>
-                    <div className='grid lg:grid-cols-8 gap-4 popular-movie-section'>
+                    <h1 className='text-2xl text-white py-6'>PIONEERFLIX KIDS</h1>
+                    <div className='grid lg:grid-cols-4 gap-5 popular-movie-section'>
 
                         {
                             popularMovies.map(movie =>
@@ -84,27 +83,16 @@ const TvChannels = () => {
             <div className='text-container bg-primary grid lg:grid-cols-2 gap-4 items-center sm:grid-cols-1'>
 
                 <div className='text-container-left'>
-                    <h1 className='text-5xl font-bold text-white'>Enjoy on your TV.</h1>
-                    <p className='text-white text-2xl'>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</p>
+                    <h1 className='text-5xl font-bold text-white'>Create profiles for kids.</h1>
+                    <p className='text-white text-2xl'>Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</p>
                 </div>
 
-                <div className='our-story-card-animation-container pb-20'>
-                    <img className='tv-img' src={tv} alt="" />
-
-                    <div className='our-story-card-animation' data-uia="our-story-card-animation">
-
-                        <video className='our-story-card-video'
-
-                            data-uia="our-story-card-video" autoPlay playsinline muted loop>
-                            <source src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v' type='video/mp4' />
-
-                        </video>
-                    </div>
-
+                <div>
+                    <img src={profileKids} alt="" />
                 </div>
             </div>
         </section>
     );
 };
 
-export default TvChannels;
+export default PioneerflixKids;
