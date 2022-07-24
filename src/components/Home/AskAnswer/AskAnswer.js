3f9b8cf7ee260cf28e1f7586668b6218a25cdb3c
Saiyadul77 <saiyadul77@gmail.com>
2022-07-25 01:32:06 +0600

    created frequently ask and answer page

---
+++ src/components/Home/AskAnswer/AskAnswer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AskAnswer = () => {
+    return (
+        <div>
+            <h1>This is Frequently ask and answer page.</h1>
+        </div>
+    );
+};
+
+export default AskAnswer;
