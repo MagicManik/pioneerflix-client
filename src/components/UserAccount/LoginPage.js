9466ed456d763cd7a0d793658d06c77633d1a9a5
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-07-29 00:24:34 +0600

    google login are working

--- src/components/UserAccount/LoginPage.js
+++ src/components/UserAccount/LoginPage.js
@@ -1,29 +1,75 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading/Loading';
 import './LoginPage.css';
 import SocialLogin from './SocialLogin/SocialLogin';
 
 const LoginPage = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const navigate = useNavigate();
+    let location = useLocation();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    let from = location.state?.from?.pathname || "/";
+
+    let errorMessage;
+
+    useEffect(() => {
+
+        if (user) {
+            navigate(from, { replace: true });
+        }
+
+    }, [user, navigate, from])
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (error) {
+        errorMessage = <p className='text-red-500'><small>{error?.message}</small></p>
+    }
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password);
+        signInWithEmailAndPassword(email, password);
+        navigate(from, { replace: true });
+    }
+
     return (
         <div>
             <div className='loginRoot'>
                 <div>
-                    <form action="" className='loginBody'>
+                    <form action="" className='loginBody' onSubmit={handleLogin}>
                         <h1 className='text-2xl text-white border-2 border-none uppercase'>Please Login</h1>
                         <div className='loginBox'>
-                            <input type="text" required />
+                            <input ref={emailRef} type="text" required />
                             <span className=''>your email</span>
                         </div>
                         <div className='loginBox'>
-                            <input type="password" required />
+                            <input ref={passwordRef} type="password" required />
                             <span className=''>Password</span>
                         </div>
+                        {errorMessage}
                         <div className='button_3_body'>
                             <input type="submit" value="LOGIN" className='button_3' />
                         </div>
                     </form>
                 </div>
                 <div className="divider text-white">OR</div>
-
                 <SocialLogin></SocialLogin>
             </div>
         </div>
--- src/components/UserAccount/SocialLogin/SocialLogin.js
+++ src/components/UserAccount/SocialLogin/SocialLogin.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSignInWithGoogle, useSignInWithFacebook } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const [signInWithFacebook, userFB, loadingFB, errorFB] = useSignInWithFacebook(auth);
 
-    if(user || userFB){
-        console.log(user, userFB);
+    let location = useLocation();
+    const navigate = useNavigate();
+
+    let from = location.state?.from?.pathname || "/";
+
+    let errorMessage;
+
+    useEffect( () => {
+
+        if (user || userFB) {
+            navigate(from, { replace: true });
+        }
+
+    }, [user, userFB, navigate, from])
+
+    if (loading || loadingFB) {
+        return <Loading></Loading>
+    }
+
+    if (error || errorFB) {
+        errorMessage = <p className='text-danger'>Error: {error?.message} {errorFB?.message}</p>
     }
 
     return (
         <div>
             <div className='social-button '>
+                {errorMessage}
                 <button
                     onClick={() => signInWithGoogle()}
                     className="button_3 justify-center mx-auto uppercase"
