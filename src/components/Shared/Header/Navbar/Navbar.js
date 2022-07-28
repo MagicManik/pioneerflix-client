f5ea68bdd2b55b7198290a1a6604f954bd3f89ae
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-07-28 18:26:37 +0600

    firebase setup

--- src/components/Shared/Header/Navbar/Navbar.js
+++ src/components/Shared/Header/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -10,9 +11,12 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 text-red-500 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
                         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Homepage</a></li>
+                            {/* <li><a>Homepage</a></li>
                             <li><a>Portfolio</a></li>
-                            <li><a>About</a></li>
+                            <li><a>About</a></li> */}
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/login'>Login</Link></li>
+                            <li><Link to='/signup'>SignUp</Link></li>
                         </ul>
                     </div>
                 </div>
