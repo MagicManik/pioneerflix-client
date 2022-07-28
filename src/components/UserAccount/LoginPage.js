498e797ac69298e4ff521ada2b7244ef536a3544
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-07-28 14:10:36 +0600

    login page design complete

--- src/components/UserAccount/LoginPage.js
+++ src/components/UserAccount/LoginPage.js
@@ -5,15 +5,23 @@ const LoginPage = () => {
     return (
         <div>
             <div className='loginRoot'>
-                <div className='loginBody'>
-                    <div className='loginBox'>
-                        <input type="text" required />
-                        <span className=''>your email</span>
-                    </div>
-                    <div className='loginBox'>
-                        <input type="password" required />
-                        <span className=''>Password</span>
-                    </div>
+                <div >
+
+                    <form action="" className='loginBody'>
+                        <h1 className='text-2xl text-white border-l border-none'>Please Login</h1>
+                        <div className='loginBox'>
+                            <input type="text" required />
+                            <span className=''>your email</span>
+                        </div>
+                        <div className='loginBox'>
+                            <input type="password" required />
+                            <span className=''>Password</span>
+                        </div>
+                        <div className='button_3_body'>
+                            <input type="submit" value="LOGIN" className='button_3' />
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </div>
