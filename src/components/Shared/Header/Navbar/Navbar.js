4abe9332563d11f7cf7cd0d8ec85548ef6818407
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-07-29 02:13:11 +0600

    change code

--- src/components/Shared/Header/Navbar/Navbar.js
+++ src/components/Shared/Header/Navbar/Navbar.js
@@ -22,13 +22,7 @@ const Navbar = () => {
                         </label>
 
                         <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            {/* <li><Link to={'/'}>Home</Link></li>
-                            <li><a>Portfolio</a></li>
-                            <li><Link to={'/about'}>About</Link></li> */}
                             <li><Link to='/'>Home</Link></li>
-                            <li><Link to='/login'>login</Link></li>
-                            <li><Link to='/signup'>signup</Link></li>
-
                             {
                                 user
                                     ?
