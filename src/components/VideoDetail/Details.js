b9c1fae843ae77d4fa43b3243fe6e8c1ffad9e05
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-07-30 18:19:52 +0600

    responsive video details page

--- src/components/VideoDetail/Details.js
+++ src/components/VideoDetail/Details.js
@@ -51,12 +51,12 @@ const Details = () => {
   }
 
   return (
-    <div className="px-14 pt-3 bg-primary">
+    <div className="md:px-14 px-3 pt-3 bg-primary">
       <div className="justify-center flex ">
         <iframe
-          className="rounded-sm p-1 shadow-2xl border-2 border-zinc-700 "
+          className="rounded-sm h-full md:h-[700px] md:p-1 shadow-2xl border-2 border-zinc-700 "
           width="100%"
-          height="700px"
+         
           src="https://www.youtube.com/embed/M3xjz4nxzGQ"
           title="YouTube video player"
           frameborder="0"
@@ -65,19 +65,19 @@ const Details = () => {
         ></iframe>
       </div>
 
-      <div className="grid  grid-cols-6  py-8">
-        <div className=" col-start-1 col-end-3 w-full">
+      <div className="grid  md:grid-cols-6  py-8">
+        <div className=" col-start-1 md:col-end-3 col-end-7 flex md:justify-start justify-center items-center w-full">
           <img
             src={movie4}
-            className="w-[350px] h-[500px]  border-[1px] border-white "
+            className="md:w-[350px] md:h-[500px] h-3/5  border-[1px] border-white "
             alt=""
           />
         </div>
-        <div className=" mt-5 col-start-3 ml-[-40px] col-end-12 ">
+        <div className=" md:mt-5 md:col-start-3 col-start-7 col-end-12 md:ml-[-40px] ml-5 ">
           <div className=" text-white">
             <div>
-              <h1 class="text-5xl font-semibold">Mogoje Mohaproloy</h1>
-              <hr className="mt-6 mb-4" />
+              <h1 class="md:text-5xl text-lg md:font-semibold">Mogoje Mohaproloy</h1>
+              <hr className="md:mt-6 my-4 md:mb-4" />
 
               <p className="text-sm">( 2022 )  . 0 hr 4 min . Arabic </p>
               <p className="my-2 text-sm"> Type : Song</p>
@@ -86,7 +86,7 @@ const Details = () => {
                 excepturi exercitationem quasi. In deleniti eaque aut repudiandae
                 et a id nisi.
               </p>
-              <hr className="mt-6 mb-4" />
+              <hr className="md:mt-6 my-3 md:mb-4" />
               <div className="flex items-center ">
                 <p>{like}</p>
 
@@ -94,8 +94,8 @@ const Details = () => {
 
 
               </div>
-              <div className="mt-5 ">
-                <textarea placeholder="Please Write Your Comment" className="p-3 text-black border-2 rounded-sm border-zinc-700" name="" id="" cols="65" rows="4"></textarea> <br />
+              <div className="mt-5 hidden md:block">
+                <textarea placeholder="Please Write Your Comment" className="p-3 w-3/4 text-black border-2 rounded-sm border-zinc-700" name="" id=""  rows="4"></textarea> <br />
                 <button className="  bg-amber-500 px-7 rounded-sm py-2 mt-2 text-xl">Submit</button>
               </div>
 
@@ -105,24 +105,31 @@ const Details = () => {
         </div>
       </div>
 
-      <div className="grid  grid-cols-6 gap-5  ">
-        <div className="w-[350px]  col-start-1  col-end-3 ">
+
+
+      <div className="grid  md:grid-cols-6 gap-5  ">
+        <div className="md:w-[350px] w-full md:col-start-1  md:col-end-3 ">
           <div className="text-white" >
             <button className="bg-amber-500 py-3 px-6 ">Add To My List</button>
-            <button className="border-2 border-amber-500 py-2 ml-2 px-6">Share</button>
-            <button className="border-2 border-amber-500 py-3 ml-2 px-6"><FaEllipsisH /></button>
+            <button className="border-2 border-amber-500 py-2 md:ml-2 ml-3 px-7 md:px-6">Share</button>
+            <button className="border-2 border-amber-500 py-3 md:ml-2 ml-4 px-7 md:px-6"><FaEllipsisH /></button>
           </div>
+          {/* this is another input field for mobile device  */}
+      <div className=" block md:hidden my-5">
+                <textarea placeholder="Please Write Your Comment" className="p-3 w-full text-black border-2 rounded-sm border-zinc-700" name="" id=""  rows="4"></textarea> <br />
+                <button className="  bg-amber-500 px-7 rounded-sm py-2 mt-2 text-xl">Submit</button>
+              </div>
         </div>
-        <div className="   col-start-3 w-full  col-end-12 ">
+        <div className="   md:col-start-3 w-full  md:col-end-12 ">
           <div className=" text-white">
             <div className=" video-container">
               <h1 className="text-4xl mb-4  font-medium">
                 You May Also Like
               </h1>
-              <div className="grid lg:grid-cols-3 gap-4 ">
+              <div className="grid grid-cols-3 gap-2 md:gap-4 ">
                 {popularMovies.map((movie) => (
                   <div className="zoom-div" key={movie._id}>
-                    <img className="w-[300px] h-[400px]  border-[1px] border-white " src={movie.img} alt="" />
+                    <img className="md:w-[300px] md:h-[400px]  border-[1px] border-white " src={movie.img} alt="" />
                   </div>
                 ))}
               </div>
@@ -132,8 +139,8 @@ const Details = () => {
       </div>
 
       {/* comment section */}
-      <hr className="mt-6 mb-4 text-zinc-500" />
-      <div className="text-white grid grid-cols-3 py-10 gap-5">
+      <hr className="mt-6 md:mb-4 text-zinc-500" />
+      <div className="text-white grid md:grid-cols-3 md:py-10 md:pt-0 pt-5 gap-5">
         {
           fakeComment.map(c => <>
             <div>
