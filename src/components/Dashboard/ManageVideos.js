c5f8b80bd110aa390d0f5c6abb9fd500679ca2fc
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-04 00:29:40 +0600

    ManageVideo component designed

--- src/components/Dashboard/ManageVideos.js
+++ src/components/Dashboard/ManageVideos.js
@@ -1,9 +1,141 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const ManageVideos = () => {
+    const [user] = useAuthState(auth);
+
     return (
         <div>
-            <h1 className='text-red-500'>Only admin can manage videos</h1>
+            <div class="overflow-x-auto w-full">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>Uploaded Video</th>
+                            <th>Uploader</th>
+                            <th>Upload by admin</th>
+                            <th>Odd Videos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        <tr>
+                            <td>
+                                <div class="flex items-center space-x-3">
+                                    <div class="avatar">
+                                        <div class="mask mask-squircle w-12 h-12">
+                                            <img src={user?.photoURL} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="font-bold">Videos Title</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Name of uploader
+                                <br />
+                                <span class="badge badge-ghost badge-sm">uploaderEmail@gmail.com</span>
+                            </td>
+                            <td>
+                                <button class="btn btn-xs">Final Upload</button>
+                            </td>
+                            <th>
+                                <button class="btn btn-xs">Delete</button>
+                            </th>
+                        </tr>
+                        {/* <!-- row 2 --> */}
+                        <tr>
+                            <td>
+                                <div class="flex items-center space-x-3">
+                                    <div class="avatar">
+                                        <div class="mask mask-squircle w-12 h-12">
+                                            <img src={user?.photoURL} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="font-bold">Videos Title</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Name of uploader
+                                <br />
+                                <span class="badge badge-ghost badge-sm">uploaderEmail@gmail.com</span>
+                            </td>
+                            <td>
+                                <button class="btn btn-xs">Final Upload</button>
+                            </td>
+                            <th>
+                                <button class="btn btn-xs">Delete</button>
+                            </th>
+                        </tr>
+                        {/* <!-- row 3 --> */}
+                        <tr>
+                            <td>
+                                <div class="flex items-center space-x-3">
+                                    <div class="avatar">
+                                        <div class="mask mask-squircle w-12 h-12">
+                                            <img src={user?.photoURL} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="font-bold">Videos Title</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Name of uploader
+                                <br />
+                                <span class="badge badge-ghost badge-sm">uploaderEmail@gmail.com</span>
+                            </td>
+                            <td>
+                                <button class="btn btn-xs">Final Upload</button>
+                            </td>
+                            <th>
+                                <button class="btn btn-xs">Delete</button>
+                            </th>
+                        </tr>
+                        {/* <!-- row 4 --> */}
+                        <tr>
+                            <td>
+                                <div class="flex items-center space-x-3">
+                                    <div class="avatar">
+                                        <div class="mask mask-squircle w-12 h-12">
+                                            <img src={user?.photoURL} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="font-bold">Videos Title</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Name of uploader
+                                <br />
+                                <span class="badge badge-ghost badge-sm">uploaderEmail@gmail.com</span>
+                            </td>
+                            <td>
+                                <button class="btn btn-xs">Final Upload</button>
+                            </td>
+                            <th>
+                                <button class="btn btn-xs">Delete</button>
+                            </th>
+                        </tr>
+                    </tbody>
+                    {/* <!-- foot --> */}
+                    <tfoot>
+                        <tr>
+                            <th>Uploaded Video</th>
+                            <th>Uploader</th>
+                            <th>Upload by admin</th>
+                            <th>Odd Videos</th>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
         </div>
     );
 };
