3fbb174c820beb87600cf3c8232a4df488469fc9
muyed moktadir <muyed004@gmail.com>
2022-08-05 18:22:45 +0600

    add description

--- src/components/Home/PioneerflixKids/PioneerflixKids.js
+++ src/components/Home/PioneerflixKids/PioneerflixKids.js
@@ -133,9 +133,9 @@ const PioneerflixKids = () => {
                 <div className='bg-primary grid lg:grid-cols-2 gap-4 items-center sm:grid-cols-1'>
 
                     <div className='text-container-left-2 pb-10'>
-                        <h1 className='text-5xl font-bold '>Watch 100+ Exclusive Videos</h1>
+                        <h1 className='text-5xl font-bold '>Find Your Kid's Collections</h1>
                         <br />
-                        <p className=' text-2xl'>Send Exclusive videos on adventures with their favorite characters in a space made just for them—free with your membership.</p>
+                        <p className=' text-2xl'>Send interactive movies with their favorite characters on fantasy in a realm designed specifically for children— With a Membership!</p>
                     </div>
 
                     <div className='pb-10'>
