0471d8b0788942f94bb9fddac77ae56bee8350ed
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-10 23:38:24 +0600

    upload video in database successfully

--- src/components/Dashboard/UploadVideo.js
+++ src/components/Dashboard/UploadVideo.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import { FiUpload } from 'react-icons/fi';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
 
 const UploadVideo = () => {
+    const [user] = useAuthState(auth);
     const {
         register,
         handleSubmit,
@@ -10,13 +14,33 @@ const UploadVideo = () => {
     } = useForm();
 
     const onSubmit = (data, e) => {
-        console.log(data);
-        // const uploadedVideo = {
-
-        // } 
-        // e.target.reset();
-
+        const userUploadVideo = {
+            title: data.title,
+            description: data.description,
+            category: data.category,
+            videoLink: data.videoLink,
+            duration: data.duration,
+            imgLink: data.imgLink,
+            uploader: user.email
+        }
+        // console.log(userUploadVideo);
+        const url = 'http://localhost:5000/userUploadVideo'
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userUploadVideo)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                alert('Thanks for uploading')
+                toast.success('Successfully your video uploaded!!!')
+                e.target.reset();
+            })
     };
+
     return (
         <div className='mb-20'>
             <div className='w-full mt-0'>
@@ -96,7 +120,7 @@ const UploadVideo = () => {
                         <div className="flex  gap-5 -mx-3 mb-6">
                             <div className="w-full md:w-1/2 px-3">
                                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                Duration
+                                    Duration
                                 </label>
                                 <input
                                     {...register("duration")}
