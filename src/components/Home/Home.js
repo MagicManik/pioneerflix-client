2a3ae1b23e97810ad225a8a420a77e1d4d683291
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-12 18:28:38 +0600

    react scroll to top implemented in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ScrollToTop from "react-scroll-to-top";
 import AskAnswer from "./AskAnswer/AskAnswer";
 
 import Banner from "./Banner";
@@ -10,6 +11,12 @@ import PioneerplixExclusive from "./PioneerplixExclusive/PioneerplixExclusive";
 import TvChannels from "./TvChannels/TvChannels";
 
 const Home = () => {
+
+  const style = {
+    background: "#e6020c",
+    borderRadius: "50%"
+  }
+
   return (
     <main className="z-10">
       <Banner></Banner>
@@ -19,6 +26,13 @@ const Home = () => {
       <PioneerflixKids></PioneerflixKids>
       <PioneerflixFree></PioneerflixFree>
       <AskAnswer></AskAnswer>
+      <ScrollToTop
+        smooth
+        top="100"
+        width="40"
+        height="20"
+        style={style}
+      />
     </main>
   );
 };
