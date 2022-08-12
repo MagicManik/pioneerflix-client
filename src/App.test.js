63427d369b86b099d961c38341b3b1f25856c912
Shaila Nasrin <drshaila2021@gmail.com>
2022-08-12 00:39:04 -0500

    test added 4

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
 import { render, screen } from "@testing-library/react";
-//import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+import MostPopular from "./components/Home/MostPopular/MostPopular";
+import PioneerflixFree from "./components/Home/PioneerflixFree/PioneerflixFree";
+import PioneerflixKids from "./components/Home/PioneerflixKids/PioneerflixKids";
+import PioneerplixExclusive from "./components/Home/PioneerplixExclusive/PioneerplixExclusive";
 import TvChannels from "./components/Home/TvChannels/TvChannels";
 
-test("renders learn react link", () => {
+test("tv channel header", () => {
   render(<TvChannels />);
-  const linkElement = screen.getByText(/TV CHANNELS/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/TV CHANNELS/i);
+  expect(textElement).toBeInTheDocument();
+});
+test("most pupular header", () => {
+  render(<MostPopular />);
+  const textElement = screen.getByText(/MOST POPULAR/i);
+  expect(textElement).toBeInTheDocument();
+});
+// test("exclusive header", () => {
+//   render(
+//     <MemoryRouter>
+//       <PioneerplixExclusive />
+//     </MemoryRouter>
+//   );
+//   const textElement = screen.getByText(/EXCLUSIVE/i);
+//   expect(textElement).toBeInTheDocument();
+// });
+test("pioneerflix kids header", () => {
+  render(
+    <MemoryRouter>
+      <PioneerflixKids />
+    </MemoryRouter>
+  );
+  const textElement = screen.getByText(/PIONEERFLIX KIDS/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test("pioneerflix free header", () => {
+  render(
+    <MemoryRouter>
+      <PioneerflixFree />
+    </MemoryRouter>
+  );
+  const textElement = screen.getByText(/PIONEERFLIX FREE/i);
+  expect(textElement).toBeInTheDocument();
 });
