1c4e9e6c9f22bac8b4724d43e07e1aa9a48d967e
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-24 17:05:20 +0600

    transaction id stored in database

--- src/components/Payments/CheckoutForm.js
+++ src/components/Payments/CheckoutForm.js
@@ -7,13 +7,15 @@ const CheckoutForm = ({ userBookingData }) => {
     const elements = useElements();
     const [cardError, setCardError] = useState('');
     const [success, setSuccess] = useState('');
+    const [processing, setProcessing] = useState(false);
     const [transactionId, setTransactionId] = useState('');
     const [clientSecret, setClientSecret] = useState('');
 
     const price = userBookingData[0]?.taka;
     const userEmail = userBookingData[0]?.userEmail;
     const userName = userBookingData[0]?.userName;
-    // console.log(price);
+    const id = userBookingData[0]?._id;
+    // console.log(id);
 
     useEffect(() => {
         fetch('https://infinite-island-65121.herokuapp.com/create-payment-intent', {
@@ -50,6 +52,7 @@ const CheckoutForm = ({ userBookingData }) => {
 
         setCardError(error?.message || '');
         setSuccess('');
+        setProcessing(true);
 
         // confirm card payment
         const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
@@ -67,11 +70,33 @@ const CheckoutForm = ({ userBookingData }) => {
 
         if (intentError) {
             setCardError(intentError?.message)
+            setProcessing(false);
         }
         else {
             setCardError('')
             setTransactionId(paymentIntent?.id)
             setSuccess('Congrats! Your payment is complemented.')
+            
+            // store payment on database
+            const payment = {
+                id: id,
+                transactionId: paymentIntent?.id
+            }
+            const url = `http://localhost:5000/booking/${id}`;
+            console.log(url);
+            console.log(payment);
+            fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(payment)
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                setProcessing(false);
+                console.log(data);
+            })
         }
         event.target.reset();
     }
