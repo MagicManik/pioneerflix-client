c45259651839cb2c6e6698ae848c4c2ea7af615b
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-26 03:15:31 +0600

    updated

--- src/components/Dashboard/DbModal.js
+++ src/components/Dashboard/DbModal.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const DbModal = () => {
+
     return (
         <div>
             <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-            <div class="modal">
+            <div class="modal" style={{ alignSelf: "flex-end", cursor: "pointer", color: "white" }}>
                 <div class="modal-box relative">
                     <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <p class="text-lg text-red-600 font-bold">Lorem ipsum dolor sit amet.</p>
+                    <div className="chartContainer">
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Dashboard/DbSingleMainPageCard.js
+++ src/components/Dashboard/DbSingleMainPageCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import DbModal from './DbModal';
 
 const DbSingleMainPageCard = ({ data }) => {
     const { title, backGround, boxShadow, barValue, update, count } = data;
@@ -8,25 +7,22 @@ const DbSingleMainPageCard = ({ data }) => {
             style={{ background: `${backGround}`, boxShadow: `${boxShadow}`, cursor: 'pointer' }}
             className="card w-full text-white"
         >
-            <label for="my-modal-3">
-                <div className={`flex p-1 duration-300 hover:bg-[#b33e50]`}>
-                    <div className='w-full flex-col '>
-                        <div
-                            className="radial-progress mx-auto ml-2 my-2 text-2xl text-white"
-                            style={{ '--value': `${barValue}`, '--thickness': '4px', '--size': '4rem' }}
-                        >
-                            {barValue}
-                        </div>
-                        <p className="card-title ml-2 uppercase">{title}</p>
+            <div className={`flex p-1 duration-300 hover:bg-[#b33e50]`}>
+                <div className='w-full flex-col '>
+                    <div
+                        className="radial-progress mx-auto ml-2 my-2 text-2xl text-white"
+                        style={{ '--value': `${barValue}`, '--thickness': '4px', '--size': '4rem' }}
+                    >
+                        {barValue}
                     </div>
-                    <div className='w-full flex-col'>
-                        <data.icon className='flex w-full justify-end ml-8 text-4xl mt-2' />
-                        <span className='flex w-full justify-end my-2 pr-4'>{count}</span>
-                        <p className='flex w-full justify-end text-sm pr-4 mt-2'>{update}</p>
-                    </div>
+                    <p className="card-title ml-2 uppercase">{title}</p>
                 </div>
-            </label>
-            <DbModal data={data} />
+                <div className='w-full flex-col'>
+                    <data.icon className='flex w-full justify-end ml-8 text-4xl mt-2' />
+                    <span className='flex w-full justify-end my-2 pr-4'>{count}</span>
+                    <p className='flex w-full justify-end text-sm pr-4 mt-2'>{update}</p>
+                </div>
+            </div>
 
         </div>
     );
