426d8b23ce580e0c11c0f240b63d537ecd7316b7
Shaila Nasrin <drshaila2021@gmail.com>
2022-08-29 12:57:52 -0500

    loader1 added

---
+++ src/components/Shared/Loader/Loader1.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loader1 = () => {
+  return (
+    <div className="page">
+      <div className="item">
+        <i class="loader --7"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Loader1;
