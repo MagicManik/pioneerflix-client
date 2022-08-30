1b220480a83e004a011ad2d77d9f5c1753158588
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-08-30 21:53:20 +0600

    user profile data update using redux

--- src/services/post.js
+++ src/services/post.js
@@ -22,9 +22,39 @@ export const postApi = createApi({
         method: 'DELETE'
       }),
     }),
+
+    // post video by admin for dashboard stored main videosCollection { mohiuddin }
+    uploadByAdmin: builder.mutation({
+      query: (userUploadVideo) => ({
+        url: 'adminUploadVideo',
+        method: 'POST',
+        body: userUploadVideo,
+        headers: {
+          'content-type': 'application/json; charset=UTF-8',
+        }
+      }),
+    }),
+
+    // update user profile in my profile route { mohiuddin }
+    updateUserProfile: builder.mutation({
+      query: (userProfile) => {
+        const {profileEmail, ...data} = userProfile;
+        console.log({profileEmail, data});
+        return {
+          url: `userProfile/${profileEmail}`,
+          method: 'PUT',
+          body: data,
+          headers: {
+            'content-type' : 'application/json; charset=UTF-8',
+          }
+        }
+      },
+    }),
+
+
   }),
 
 })
 
 // Export hooks for usage in functional components, which are auto-generated based on the defined endpoints
-export const { useGetAllVideosQuery, useDeleteUiVideoMutation } = postApi;
+export const { useGetAllVideosQuery, useDeleteUiVideoMutation, useUploadByAdminMutation, useUpdateUserProfileMutation } = postApi;
